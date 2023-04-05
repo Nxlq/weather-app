0c3c43819f6c25e2236988c4afe250ae98f71900
JeffW <116536323+Nxlq@users.noreply.github.com>
2023-04-04 20:34:00 -0400

    change: code now renders relevant weather display bg gifs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import fetchCurrentWeather from "./weather-data";
-import renderWeatherDisplay from "./weather-display-view";
+import renderWeatherDisplay, { fetchWeatherBG } from "./weather-display-view";
+import { renderWeatherDisplay } from "./weather-display-view";
+
+async function locationReqSuccess(position) {
+  const { latitude, longitude } = position.coords;
+  const currentData = await fetchCurrentWeather(`${latitude},${longitude}`);
+
+  renderWeatherDisplay(currentData);
+  return currentData;
+  // return `${latitude},${longitude}`;
+}
+
+function locationReqFailed() {
+  alert("Unable to retrieve position :( Try searching for a location!");
+}
 
 window.addEventListener("DOMContentLoaded", async () => {
-  const data = await fetchCurrentWeather();
-  console.log(data);
-  renderWeatherDisplay(data);
+  navigator.geolocation.getCurrentPosition(
+    locationReqSuccess,
+    locationReqFailed
+  );
 });
--- src/weather-data.js
+++ src/weather-data.js
@@ -1,8 +1,9 @@
-async function fetchCurrentWeather() {
+async function fetchCurrentWeather(locationQuery) {
   const response = await fetch(
-    "http://api.weatherapi.com/v1/current.json?key=9075e974d8924af5855180341233103&q=11209&aqi=no",
+    `http://api.weatherapi.com/v1/current.json?key=9075e974d8924af5855180341233103&q=${locationQuery}&aqi=no`,
     { mode: "cors" }
   );
+
   const currentData = await response.json();
 
   // add parsed info onto currentData object
