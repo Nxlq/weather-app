9f26fd967362e65ddf943f01e46c8dc706cb177c
JeffW <116536323+Nxlq@users.noreply.github.com>
2023-04-04 20:33:08 -0400

    change refactor display view coe order

--- src/weather-display-view.js
+++ src/weather-display-view.js
@@ -1,53 +1,11 @@
-function generateWeatherDisplayDom(data) {
-  console.log(data);
-  // generate weather display container
-  const weatherDisplay = document.createElement("div");
-  weatherDisplay.id = "weather-display";
-
-  // generate icon for weather display
-  const weatherIcon = document.createElement("img");
-  weatherIcon.id = "weather-display-icon";
-  weatherIcon.src = `/weather-icons/${data.current.is_day ? "day" : "night"}/${
-    data.formattedInfo.weatherIcon
-  }`;
-
-  // generate weather/location display
-  const tempAndLocationContainer = document.createElement("div");
-  tempAndLocationContainer.id = "container-weather-location-info";
-
-  const temperature = document.createElement("span");
-  temperature.id = "temperature-display";
-  temperature.textContent = `${Math.round(data.current.temp_f)}°F`;
-
-  const location = document.createElement("span");
-  location.id = "location-display";
-  location.textContent = `${data.location.name}, ${data.location.region}, ${data.location.country}`;
-
-  tempAndLocationContainer.append(weatherIcon, temperature, location);
-
-  // generate time and date info
-  const timeAndDateContainer = document.createElement("div");
-  timeAndDateContainer.id = "container-date-info";
-
-  const time = document.createElement("span");
-  time.id = "time-display";
-  time.textContent = `${data.formattedInfo.time}`;
-
-  const date = document.createElement("span");
-  date.id = "date-display";
-  date.textContent = `${data.formattedInfo.monthAbbreviation} ${data.formattedInfo.dayOfMonth}, ${data.formattedInfo.day}`;
-
-  timeAndDateContainer.append(time, date);
-
-  weatherDisplay.append(tempAndLocationContainer, timeAndDateContainer);
-
-  return weatherDisplay;
-}
-
-function renderWeatherDisplay(data) {
-  const appContent = document.getElementById("app-content");
-  appContent.append(generateWeatherDisplayDom(data));
-  setWeatherBackgroundImage(data);
+export async function fetchWeatherGif(string) {
+  const response = await fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=wbrSw8VyWBlLS4akOVt1qa9fZP9rXC40&s=${string}`,
+    { mode: "cors" }
+  );
+  const data = await response.json();
+  const gifURL = data.data.images.original.url;
+  return gifURL;
 }
 
 function updateWeatherBG(url) {
@@ -113,14 +71,54 @@ async function setWeatherBackgroundImage(data) {
   }
 }
 
-export async function fetchWeatherGif(string) {
-  const response = await fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=wbrSw8VyWBlLS4akOVt1qa9fZP9rXC40&s=${string}`,
-    { mode: "cors" }
-  );
-  const data = await response.json();
-  const gifURL = data.data.images.original.url;
-  return gifURL;
+function generateWeatherDisplayDom(data) {
+  console.log(data);
+  // generate weather display container
+  const weatherDisplay = document.createElement("div");
+  weatherDisplay.id = "weather-display";
+
+  // generate icon for weather display
+  const weatherIcon = document.createElement("img");
+  weatherIcon.id = "weather-display-icon";
+  weatherIcon.src = `/weather-icons/${data.current.is_day ? "day" : "night"}/${
+    data.formattedInfo.weatherIcon
+  }`;
+
+  // generate weather/location display
+  const tempAndLocationContainer = document.createElement("div");
+  tempAndLocationContainer.id = "container-weather-location-info";
+
+  const temperature = document.createElement("span");
+  temperature.id = "temperature-display";
+  temperature.textContent = `${Math.round(data.current.temp_f)}°F`;
+
+  const location = document.createElement("span");
+  location.id = "location-display";
+  location.textContent = `${data.location.name}, ${data.location.region}, ${data.location.country}`;
+
+  tempAndLocationContainer.append(weatherIcon, temperature, location);
+
+  // generate time and date info
+  const timeAndDateContainer = document.createElement("div");
+  timeAndDateContainer.id = "container-date-info";
+
+  const time = document.createElement("span");
+  time.id = "time-display";
+  time.textContent = `${data.formattedInfo.time}`;
+
+  const date = document.createElement("span");
+  date.id = "date-display";
+  date.textContent = `${data.formattedInfo.monthAbbreviation} ${data.formattedInfo.dayOfMonth}, ${data.formattedInfo.day}`;
+
+  timeAndDateContainer.append(time, date);
+
+  weatherDisplay.append(tempAndLocationContainer, timeAndDateContainer);
+
+  return weatherDisplay;
 }
 
-export default renderWeatherDisplay;
+export function renderWeatherDisplay(data) {
+  const appContent = document.getElementById("app-content");
+  appContent.append(generateWeatherDisplayDom(data));
+  setWeatherBackgroundImage(data);
+}
